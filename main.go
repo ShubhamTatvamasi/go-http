13c00c84f39cf82b6a2c6a5e2308b79f72730aef
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2020-05-30 01:21:46 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "strconv"
+)
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+		number := 0
+		number++
+    fmt.Fprintf(w, "Welcome to go http " + strconv.Itoa(number) + "\n")
+}
+
+func main() {
+
+
+    http.HandleFunc("/", handler)
+
+    log.Fatal(http.ListenAndServe(":80", nil))
+
+}
